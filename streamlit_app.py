ffc03fb329574469441e8cb2bde65366fe85176a
PAS5357 <54723551+PAS5357@users.noreply.github.com>
2025-01-09 11:36:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,9 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")    #added to get ready for SniS
 #session = get_active_session()    --removed to get ready for SniS
 session = cnx.session()            #added to get ready for SniS
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
